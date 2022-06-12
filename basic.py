9f281766c2288812083b1e493e68c98bd4a8bc54
sakshi3326 <saqkshiiitian123@gmail.com>
2022-06-13 01:08:22 +0530

    dilate and eroding

--- basic.py
+++ basic.py
@@ -1,4 +1,6 @@
 import cv2 as cv
+from cv2 import erode
+from numpy import ediff1d
 img = cv.imread('photos/cat.jpg')
 cv.imshow('cat',img)
 #converting to grayscale
@@ -6,10 +8,16 @@ gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('gay',gray)
 
 #blur
-blur = cv.GaussianBlur(img, (9,9), cv.BORDER_DEFAULT )
+blur = cv.GaussianBlur(img, (3,3), cv.BORDER_DEFAULT )
 cv.imshow('Blur',blur)
 
 #eddge cascade
 edge = cv.Canny(img,125,175)
 cv.imshow('Edge',edge)
+#Dilate
+dilate = cv.dilate(edge,(7,7),iterations=3)
+cv.imshow('Dilate',dilate)
+#Eroding
+erode = cv.erode(dilate,(7,7),iterations=3)
+cv.imshow('Erode',erode)
 cv.waitKey(0)
