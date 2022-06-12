4692fef647b92d8223aef7aa6d96370b8cc63c96
sakshi3326 <saqkshiiitian123@gmail.com>
2022-06-13 00:35:41 +0530

    edge cascade

--- basic.py
+++ basic.py
@@ -9,4 +9,7 @@ cv.imshow('gay',gray)
 blur = cv.GaussianBlur(img, (9,9), cv.BORDER_DEFAULT )
 cv.imshow('Blur',blur)
 
+#eddge cascade
+edge = cv.Canny(img,125,175)
+cv.imshow('Edge',edge)
 cv.waitKey(0)
